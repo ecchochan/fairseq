b101a56dcfbe7e58c955a14a7937ba282f819593
ecchochan <ecchochan@gmail.com>
2020-05-10 17:51:21 +0800

    update

--- fairseq/models/roberta/model_span_qa.py
+++ fairseq/models/roberta/model_span_qa.py
@@ -125,7 +125,7 @@ class RobertaQAModel(FairseqLanguageModel):
             args.max_positions = args.tokens_per_sample
 
         encoder = RobertaQAEncoder(args, task.source_dictionary)
-        if self.args.mixout:
+        if cls.args.mixout:
             encoder = encoder.apply(MixoutWrapper)
         return cls(args, encoder)
 
