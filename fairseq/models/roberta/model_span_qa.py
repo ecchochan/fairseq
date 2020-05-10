8a3a8ee13fd909eba0a309cc4f8e5daedbe9e288
ecchochan <ecchochan@gmail.com>
2020-05-10 22:42:59 +0800

    update

--- fairseq/models/roberta/model_span_qa.py
+++ fairseq/models/roberta/model_span_qa.py
@@ -93,7 +93,8 @@ class RobertaQAModel(FairseqLanguageModel):
         
     def apply_mixout(self, p):
         from fairseq.optim.mixout import MixoutWrapper
-        self.decoder.sentence_encoder.apply(MixoutWrapper, p)
+        from functools import partial
+        self.decoder.sentence_encoder.apply(partial(MixoutWrapper, p))
 
     @property
     def supported_targets(self):
