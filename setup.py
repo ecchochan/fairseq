17731f500336a07223975a8050c7714440b00149
ecchochan <ecchochan@gmail.com>
2020-05-11 11:28:52 +0800

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-
+# distutils: define_macros=NPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION
 from Cython.Build import cythonize
 
 import os
